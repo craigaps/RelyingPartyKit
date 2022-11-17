cfe8603e72f0dd904e490bbaef62d4b2c1594f8c
Craig Pearson <craig@syncme.com.au>
2022-11-17 12:01:15 +1000

    Allow `Token` to be `Codable`.

--- Sources/RelyingPartyKit/Models/Token.swift
+++ Sources/RelyingPartyKit/Models/Token.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 /// Represents an access token.
-public struct Token: Decodable {
+public struct Token: Codable {
     /// The access token that is issued by the authorization server.
     public let accessToken: String
     
