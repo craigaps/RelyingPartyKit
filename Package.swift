6fde3db5f14420aae2c58a07958ef4cb494b277a
Craig Pearson <craig@syncme.com.au>
2022-11-18 09:41:12 +1000

    Support for iOS 15.6

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "RelyingPartyKit",
     platforms: [
         .macOS("12.0"),
-        .iOS("16.0")
+        .iOS("15.6")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
